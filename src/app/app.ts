23c7256e2739dfdd001be2eb9703f6f866d37958
Albert <alsorribes@gmail.com>
2025-07-16 14:40:11 +0200

    Implementació de usuari, estat i avatar

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { Settings } from './settings/settings';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Settings],
+  imports: [Settings],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
